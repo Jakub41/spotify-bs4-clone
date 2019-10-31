b07e43d66a9a85eaa30c90968ff86d9d04ebfe1c
jakub41 <lemiszewski@gmx.com>
2019-10-31 15:45:07 +0100

    Added basic auto collapse on windows resize

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-$(function(){
-    // Sidebar collapse
-    $('.btn-toggle-sidebar').on('click', function(){
-        $('.wrapper').toggleClass('sidebar-collapse');
-    });
-    $('.sb-item').on('click', function(){
-        if ($(this).hasClass('btn-toggle-sidebar')) return;
-        $(this).siblings().removeClass('active');
-        $(this).siblings().find('.sb-item').removeClass('active');
-        $(this).addClass('active');
-    });
+$(function() {
+  // Sidebar collapse
+  $(".btn-toggle-sidebar").on("click", function() {
+    $(".wrapper").toggleClass("sidebar-collapse");
+  });
 
+  // Sidebars items
+  $(".sb-item").on("click", function() {
+    if ($(this).hasClass("btn-toggle-sidebar")) return;
+    $(this)
+      .siblings()
+      .removeClass("active");
+    $(this)
+      .siblings()
+      .find(".sb-item")
+      .removeClass("active");
+    $(this).addClass("active");
+  });
+
+  // Adding the collapse when window < 768 tablets and small devices
+  $(window).on("resize", function() {
+    var win = $(this);
+    if (win.width() < 768) {
+      $(".wrapper").addClass("sidebar-collapse");
+    } else {
+      $(".wrapper").removeClass("sidebar-collapse");
+    }
+  });
 });
